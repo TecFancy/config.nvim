dabbbf579d9a9ebd6d8c07897bf3a73ce8c1a758
smpower <rf.wangchn@foxmail.com>
2023-09-23 22:41:56 +0800

    feat: config folder structure

--- init.lua
+++ init.lua
@@ -1,19 +1,5 @@
 require("randal.core.options")
 require("randal.core.keymaps")
 require("randal.core.colorscheme")
-
--- use lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("randal.plugins-setup")
+require("randal.plugins-setup")
 
