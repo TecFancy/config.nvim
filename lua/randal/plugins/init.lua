380b891505d438a79714d72965ab0b3028a1c928
smpower <rf.wangchn@foxmail.com>
2023-09-25 01:23:32 +0800

    fix: dap debug configuration

--- lua/randal/plugins/init.lua
+++ lua/randal/plugins/init.lua
@@ -105,7 +105,191 @@ return {
   "lewis6991/gitsigns.nvim",
 
   -- debug
-  "puremourning/vimspector"
+  -- "puremourning/vimspector"
+
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "mxsdev/nvim-dap-vscode-js",
+      -- build debugger from source
+      {
+        "microsoft/vscode-js-debug",
+        version = "1.x",
+        build = "npm i && npm run compile vsDebugServerBundle && mv dist out"
+      }
+    },
+    keys = {
+      -- normal mode is default
+      { "<leader>dd", function() require 'dap'.toggle_breakpoint() end },
+      { "<leader>dc", function() require 'dap'.continue() end },
+      { "<C-'>",     function() require 'dap'.step_over() end },
+      { "<C-;>",     function() require 'dap'.step_into() end },
+      { "<C-:>",     function() require 'dap'.step_out() end },
+    },
+    config = function()
+      require("dap-vscode-js").setup({
+        debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+      })
+
+      for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "svelte" }) do
+        require("dap").configurations[language] = {
+          -- attach to a node process that has been started with
+          -- `--inspect` for longrunning tasks or `--inspect-brk` for short tasks
+          -- npm script -> `node --inspect-brk ./node_modules/.bin/vite dev`
+          {
+            -- use nvim-dap-vscode-js's pwa-node debug adapter
+            type = "pwa-node",
+            -- attach to an already running node process with --inspect flag
+            -- default port: 9222
+            request = "attach",
+            -- allows us to pick the process using a picker
+            processId = require 'dap.utils'.pick_process,
+            -- name of the debug action you have to select for this config
+            name = "Attach debugger to existing `node --inspect` process",
+            -- for compiled languages like TypeScript or Svelte.js
+            sourceMaps = true,
+            -- resolve source maps in nested locations while ignoring node_modules
+            resolveSourceMapLocations = {
+              "${workspaceFolder}/**",
+              "!**/node_modules/**" },
+              -- path to src in vite based projects (and most other projects as well)
+              cwd = "${workspaceFolder}/src",
+              -- we don't want to debug code inside node_modules, so skip it!
+              skipFiles = { "${workspaceFolder}/node_modules/**/*.js" },
+            },
+            {
+              type = "pwa-chrome",
+              name = "Launch Chrome to debug client",
+						request = "launch",
+						url = "http://localhost:3000",
+						sourceMaps = true,
+						protocol = "inspector",
+						port = 9222,
+						webRoot = "${workspaceFolder}/src",
+						-- skip files from vite's hmr
+						skipFiles = { "**/node_modules/**/*", "**/@vite/*", "**/src/client/*", "**/src/*" },
+					},
+					-- only if language is javascript, offer this debug action
+					language == "javascript" and {
+						-- use nvim-dap-vscode-js's pwa-node debug adapter
+						type = "pwa-node",
+						-- launch a new process to attach the debugger to
+						request = "launch",
+						-- name of the debug action you have to select for this config
+						name = "Launch file in new node process",
+						-- launch current file
+						program = "${file}",
+						cwd = "${workspaceFolder}",
+					} or nil,
+				}
+			end
+	
+			require("dapui").setup()
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open({ reset = true })
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+			dap.listeners.before.event_exited["dapui_config"] = dapui.close
+		end
+	}
+
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   lazy = true,
+  --   dependencies = {
+  --     "rcarriga/nvim-dap-ui",
+  --     "mxsdev/nvim-dap-vscode-js",
+  --     -- lazy spec to build "microsoft/vscode-js-debug" from source
+  --     {
+  --       "microsoft/vscode-js-debug",
+  --       version = "1.x",
+  --       build = "npm i && npm run compile vsDebugServerBundle && mv dist out"
+  --     }
+  --   },
+  --   keys = {
+  --     { "<leader>dd", function() require('dap').toggle_breakpoint() end },
+  --     { "<leader>dc", function() require('dap').continue() end },
+  --   },
+  --   -- config = function()
+  --   --   require("dapui").setup()
+  --   -- end,
+  --   config = function()
+  --     require("dap-vscode-js").setup({
+  --       debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+  --       adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+  --     })
+  --     require("dapui").setup()
+  --
+  --     for _, language in ipairs({ "typescript", "javascript", "svelte", "typescriptreact" }) do
+  --       require("dap").configurations[language] = {
+  --         -- config goes here
+  --         -- Launch current file in new node process
+  --         {
+  --           -- use nvim-dap-vscode-js's pwa-node debug adapter
+  --           type = "pwa-node",
+  --           -- launch a new process to attach the debugger to
+  --           request = "launch",
+  --           -- name of the debug action you have to select for this config
+  --           name = "Launch current file in new node process",
+  --           program = "${file}",
+  --         },
+  --         -- Attach to an inspectable node process
+  --         -- e.g., 
+  --         -- # start SvelteKit dev server with inspectable node process
+  --         -- npm --node-options --inspect-brk run dev
+  --         -- npm --node-options --inspect run dev
+  --         {
+  --           -- use nvim-dap-vscode-js's pwa-node debug adapter
+  --           type = "pwa-node",
+  --           -- attach to an already running node process with --inspect flag
+  --           -- default port: 9222
+  --           request = "attach",
+  --           -- allows us to pick the process using a picker
+  --           processId = require 'dap.utils'.pick_process,
+  --           -- name of the debug action
+  --           name = "Attach debugger to existing `node --inspect` process",
+  --           -- for compiled languages like TypeScript or Svelte.js
+  --           sourceMaps = true,
+  --           -- resolve source maps in nested locations while ignoring node_modules
+  --           resolveSourceMapLocations = { "${workspaceFolder}/**",
+  --           "!**/node_modules/**"},
+  --           -- path to src in vite based projects (and most other projects as well)
+  --           cwd = "${workspaceFolder}/src",
+  --           -- we don't want to debug code inside node_modules, so skip it!
+  --           skipFiles = { "${workspaceFolder}/node_modules/**/*.js" },
+  --         },
+  --         -- Debug the web in google chrome
+  --         {
+  --           -- use nvim-dap-vscode-js's pwa-chrome debug adapter
+  --           type = "pwa-chrome",
+  --           request = "launch",
+  --           -- name of the debug action
+  --           name = "Launch Chrome to debug client side code",
+  --           -- default vite dev server url
+  --           url = "http://localhost:3000",
+  --           -- for TypeScript/Svelte
+  --           sourceMaps = true,
+  --           webRoot = "${workspaceFolder}/src",
+  --           protocol = "inspector",
+  --           port = 9222,
+  --           -- skip files from vite's hmr
+  --           skipFiles = { "**/node_modules/**/*", "**/@vite/*", "**/src/client/*", "**/src/*" },
+  --         },
+  --       }
+  --     end
+  --
+  --     local dap, dapui = require("dap"), require("dapui")
+  --     dap.listeners.after.event_initialized["dapui_config"] = function()
+  --       dapui.open({ reset = true })
+  --     end
+  --     dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+  --     dap.listeners.before.event_exited["dapui_config"] = dapui.close
+  --   end
+  -- }
+
   -- {
   --   "mfussenegger/nvim-dap",
   --   "Pocco81/DAPInstall.nvim",
